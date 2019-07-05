d1fb7cba12eb2125467aa1f4163ea5fbe8b90b53
Joshua Barretto <joshua.s.barretto@gmail.com>
2019-07-05 20:52:01 +0100

    Added basic Veloren display and movement

---
+++ src/display.rs
@@ -0,0 +1,66 @@
+use std::{
+    io::{self, Write, Stdout},
+};
+use vek::*;
+use termion::{
+    raw::{IntoRawMode, RawTerminal},
+    clear,
+    cursor,
+};
+
+pub struct Display {
+    size: Vec2<u16>,
+    stdout: RawTerminal<Stdout>,
+}
+
+impl Display {
+    pub fn new(size: impl Into<Vec2<u16>>, stdout: Stdout) -> Self {
+        let mut this = Self {
+            size: size.into(),
+            stdout: stdout.into_raw_mode().unwrap(),
+        };
+        this.init();
+        this
+    }
+
+    fn init(&mut self) {
+        write!(self.stdout, "{}{}", clear::All, cursor::Hide).unwrap();
+    }
+
+    pub fn clear_with(&mut self, c: char) {
+        for y in 1..=self.size.y {
+            write!(self.stdout, "{}", cursor::Goto(1, y + 1)).unwrap();
+            for _ in 1..=self.size.x {
+                write!(self.stdout, "{}", c).unwrap();
+            }
+        }
+    }
+
+    pub fn at(&mut self, pos: impl Into<Vec2<u16>>) -> DisplayAt {
+        let pos = pos.into();
+        write!(self.stdout, "{}", cursor::Goto(pos.x + 1, pos.y + 1)).unwrap();
+        DisplayAt(self)
+    }
+
+    pub fn flush(&mut self) {
+        self.stdout.flush().unwrap();
+    }
+}
+
+impl Drop for Display {
+    fn drop(&mut self) {
+        write!(self.stdout, "{}{}", clear::All, cursor::Show).unwrap();
+    }
+}
+
+pub struct DisplayAt<'a>(&'a mut Display);
+
+impl<'a> io::Write for DisplayAt<'a> {
+    fn write(&mut self, b: &[u8]) -> io::Result<usize> {
+        self.0.stdout.write(b)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.stdout.flush()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,89 @@
+mod display;
+
 use std::{
     sync::mpsc,
     thread,
-    io::{Write, stdin, stdout},
+    io::{stdin, stdout, Write},
+    net::ToSocketAddrs,
+    time::Duration,
+    process,
 };
 use vek::*;
-use terminal_graphics::{
-    Display,
-    graphics::Graphics,
-};
 use termion::{
-    raw::IntoRawMode,
     input::TermRead,
     event::Key,
-    cursor,
+    color,
 };
-
-const SCREEN_SIZE: Vec2<u32> = Vec2 {
-    x: 80,
-    y: 25,
+use clap::{App, Arg};
+use veloren_client::Client;
+use veloren_common::{
+    comp,
+    clock::Clock,
+    vol::{ReadVol, Vox},
+    terrain::{TerrainMap, Block},
 };
+use specs::Join;
+use crate::display::Display;
 
 fn main() {
-    let mut screen = Display::new(SCREEN_SIZE.x, SCREEN_SIZE.y);
-    screen.clear();
+    let screen_size = Vec2::new(80, 25);
+    let view_distance = 3;
+    let tps = 30;
 
-    let mut stdout = stdout().into_raw_mode().unwrap();
+    let matches = App::new("Teloren")
+        .version("0.1")
+        .author("Joshua Barretto <joshua.s.barretto@gmail.com>")
+        .about("A terminal Veloren client frontend")
+        .arg(Arg::with_name("alias")
+            .short("a")
+            .long("alias")
+            .value_name("ALIAS")
+            .help("Set the in-game alias")
+            .takes_value(true))
+        .arg(Arg::with_name("server")
+            .short("s")
+            .long("server")
+            .value_name("SERVER_ADDR")
+            .help("Set the server address")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .value_name("PORT")
+            .help("Set the server port")
+            .takes_value(true))
+        .get_matches();
 
+    // Find arguments
+    let server_addr = matches.value_of("server").unwrap_or("127.0.0.1");
+    let server_port = matches.value_of("port").unwrap_or("59003");
+    let alias = matches.value_of("alias").unwrap_or("teloren_user");
+
+    // Parse server socket
+    let server_sock = format!("{}:{}", server_addr, server_port)
+        .to_socket_addrs()
+        .unwrap_or_else(|_| {
+            println!("Invalid server address");
+            process::exit(1);
+        })
+        .next()
+        .unwrap();
+
+    let mut client = Client::new(server_sock, Some(view_distance))
+        .unwrap_or_else(|err| {
+            println!("Failed to connect to server: {:?}", err);
+            process::exit(1);
+        });
+
+    println!("Server info: {:?}", client.server_info);
+    println!("Players: {:?}", client.get_players());
+
+    client.register(comp::Player::new(alias.to_string(), Some(view_distance)));
+
+    client.request_character(alias.to_string(), comp::Body::Humanoid(comp::humanoid::Body::random()));
+
+    // Spawn input thread
     let stdin = stdin();
     let (key_tx, key_rx) = mpsc::channel();
     thread::spawn(move || {
@@ -35,20 +93,91 @@ fn main() {
         }
     });
 
-    let mut graphics = Graphics::new();
+    let mut display = Display::new(screen_size, stdout());
 
-    write!(stdout, "{}", cursor::Hide);
+    let mut clock = Clock::start();
 
-    'running: loop {
+    'running: for tick in 0.. {
+        let mut controller = comp::Controller::default();
+
+        // Handle inputs
         for c in key_rx.try_iter() {
             match c.unwrap() {
+                Key::Char('w') => controller.move_dir.y -= 1.0,
+                Key::Char('a') => controller.move_dir.x -= 1.0,
+                Key::Char('s') => controller.move_dir.y += 1.0,
+                Key::Char('d') => controller.move_dir.x += 1.0,
                 Key::Char('q') => break 'running,
                 _ => {},
             }
         }
 
-        graphics.draw(&mut screen);
+        // Tick client
+        client.tick(controller, clock.get_last_delta()).unwrap();
+        client.cleanup();
 
-        stdout.flush().unwrap();
+        let ecs = client.state().ecs();
+
+        // Get player pos
+        let player_pos = ecs
+            .read_storage::<comp::Pos>()
+            .get(client.entity())
+            .map(|pos| pos.0.map(|e| e.floor() as i32))
+            .unwrap_or(Vec3::zero());
+
+        // Render block
+        for j in 0..screen_size.y {
+            for i in 0..screen_size.x {
+                let wpos = player_pos + Vec3::new(i, j, 0)
+                    .map2(screen_size.into(), |e, sz: u16| e as i32 - sz as i32 / 2);
+
+                let mut block = None;
+
+                for z in -16..16 {
+                    if let Some(b) = ecs
+                        .read_resource::<TerrainMap>()
+                        .get(wpos - Vec3::unit_z() * z)
+                        .ok()
+                        .filter(|b| !b.is_empty())
+                    {
+                        block = Some(*b);
+                        break;
+                    }
+                }
+
+                let (col, c) = match block {
+                    Some(block) => match block {
+                        block if block.is_empty() => (Rgb::one(), ' '),
+                        _ => (block.get_color().unwrap_or(Rgb::one()), ' '),
+                    },
+                    None => (Rgb::zero(), '?'),
+                };
+
+                write!(display.at((i, j)), "{}{}", color::Rgb(col.r, col.g, col.b).bg_string(), c).unwrap();
+            }
+        }
+
+        let to_screen_pos = |pos: Vec2<f32>| pos.map(|e| e as i32) - Vec2::from(player_pos) + screen_size.map(|e| e as i32) / 2;
+
+        for (
+            pos,
+        ) in (
+            &ecs.read_storage::<comp::Pos>(),
+        ).join() {
+            let scr_pos = to_screen_pos(Vec2::from(pos.0));
+
+            if scr_pos
+                .map2(screen_size, |e, sz| e >= 0 && e < sz as i32)
+                .reduce_and()
+            {
+                write!(display.at((scr_pos.x as u16, scr_pos.y as u16)), "{}{}", color::Rgb(0, 0, 0).bg_string(), '@').unwrap();
+            }
+        }
+
+        // Finish drawing
+        display.flush();
+
+        // Wait for next tick
+        clock.tick(Duration::from_millis(1000 / tps));
     }
 }
